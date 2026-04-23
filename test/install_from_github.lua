e88ec46be1d402d6e9ac86621ca1e46bc24ad77b
SomethingBasic01 <144187313+SomethingBasic01@users.noreply.github.com>
2026-04-23 09:40:06 -0500

    Add files via upload

---
+++ test/install_from_github.lua
@@ -0,0 +1,42 @@
+-- Simple GitHub installer/updater for HOLO-NET v3.
+-- Edit BASE to your raw GitHub folder URL, then run:
+--   lua install_from_github.lua
+--
+-- Example BASE:
+-- https://raw.githubusercontent.com/YourUser/YourRepo/main/holonet_v3
+
+local component = require("component")
+local internet = require("internet")
+local filesystem = require("filesystem")
+
+assert(component.isAvailable("internet"), "No internet card found")
+
+local BASE = "https://raw.githubusercontent.com/REPLACE_ME/REPLACE_ME/main/REPLACE_ME"
+local FILES = {
+  "console.lua",
+  "scan_node.lua",
+  "projector_node.lua",
+  "README.txt",
+}
+
+local function fetch(url)
+  local handle, err = internet.request(url)
+  assert(handle, err or "request failed")
+  local chunks = {}
+  for chunk in handle do
+    chunks[#chunks + 1] = chunk
+  end
+  return table.concat(chunks)
+end
+
+for i = 1, #FILES do
+  local name = FILES[i]
+  local url = BASE .. "/" .. name
+  print("Downloading " .. url)
+  local data = fetch(url)
+  local f = assert(io.open("/home/" .. name, "w"))
+  f:write(data)
+  f:close()
+end
+
+print("Done. Files saved to /home/")
